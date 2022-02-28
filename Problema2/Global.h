13509f132b013b3f0942e8b76c5d7f30a311f223
stefan1044 <39203792+stefan1044@users.noreply.github.com>
2022-02-28 19:57:24 +0200

    Delete Problema2 directory

--- Problema2/Global.h
+++
@@ -1,6 +0,0 @@
-#include "Student.h"
-int compareName(Student& student1, Student& student2);
-int compareMathGrade(Student& student1, Student& student2);
-int compareEnglishGrade(Student& student1, Student& student2);
-int compareHistoryGrade(Student& student1, Student& student2);
-int compareAverageGrade(Student& student1, Student& student2);

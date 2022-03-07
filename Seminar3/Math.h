0d622289bb41092a1b6a033006e831edbb22fc93
stefan1044 <39203792+stefan1044@users.noreply.github.com>
2022-03-07 19:21:14 +0200

    Add files via upload

---
+++ Seminar3/Math.h
@@ -0,0 +1,23 @@
+class Math {
+public:
+
+    static int Add(int a, int b);
+
+    static int Add(int a, int b, int c);
+
+    static int Add(double a, double b);
+
+    static int Add(double a, double b, double c);
+
+    static int Mul(int a, int b);
+
+    static int Mul(int a, int b, int c);
+
+    static int Mul(double a, double b);
+
+    static int Mul(double a, double b, double c);
+
+    static int Add(int count, ...); // sums up a list of integers
+
+    static char* Add(const char* a, const char* b);
+};

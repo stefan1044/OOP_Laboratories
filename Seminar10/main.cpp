c4f802d8b589fd2700b80236e0ecee93adb9402f
stefan1044 <39203792+stefan1044@users.noreply.github.com>
2022-05-20 03:57:17 +0300

    Update main.cpp

--- Seminar10/main.cpp
+++ Seminar10/main.cpp
@@ -1,72 +1,74 @@
 #include "templ.h"
 #include <iostream>
 #include <exception>
+#include <vector>
 
 using namespace std;
 
-
-class exceptie1 : public exception{
-
-    virtual const char* what() const throw(){
-
-        return "Impartire la 0!";
+int desc(const int &a,const int &b) {
+    if (a == b)
+        return 0;
+    else if (a < b)
+        return 1;
+    return -1;
+}
+/*
+class compareAsc : public Compare {
+    int CompareElements(void* e1, void* e2);
+    int CompareElements(const int& e1, const int& e2) {
+        if (e1 == e2)
+            return 0;
+        else if (e1 < e2)
+            return -1;
+        return 1;
     }
+}comparator;*/
 
-};
+int main() {
 
-class exceptie2 : public exception{
+    Array<int> A(15);
+    Array<int> B(A);
+    
+    int n = 25;
+    A += n;
+    cout << A[0]<<'\n';
+    A.Insert(2, 17);
+    cout << A[2]<<'\n';
+    B.Insert(0,A);
+    cout <<B[0]<<' '<< B[2] << '\n';
+    B.Delete(2);
+    
+    bool test1 = (B = A);
+    if (test1 == true )
+        cout<<"Copied" << '\n';
+    cout << B[2] << '\n';
 
-    virtual const char* what() const throw(){
+    A += 19, A += 221;
+    A.Sort();
+    for (int i = 0; i < A.GetSize(); i++)
+        cout << A[i] << ' ';
+    cout << '\n';
+    A.Sort(desc);
+    for (int i = 0; i < A.GetSize(); i++)
+        cout << A[i] << ' ';
+    cout << '\n';
+   //compareAsc* point1;
+   //point1 = &comparator;
+   //A.Sort(point1);
+    for (int i = 0; i < A.GetSize(); i++)
+        cout << A[i] << ' ';
+    cout << '\n';
 
-        return "Indexul este inafara domeniului!";
-    }
+    cout<<A.BinarySearch(221)<<'\n';
+    cout << A.BinarySearch(221, desc) << '\n';
+    //cout<<A.BinarySearch(221, point1) << '\n';
 
-};
-
-
-
-int main(){
-
-    int z=-1, x = 50, y = 0;
-
-    int arr[4] = { 0 };
-
-    int i = 10;
-
-    exceptie1 divide0;
-
-    exceptie2 index_out_of_bounds;
-
-
-
-    try{
-
-        if (y==0) throw divide0;
-
-        z = x / y;
-
-        cout << "Fara exceptie: z=" << z << endl;
-    }
-    catch (exception& e){
-
-        cout << "Exceptie: " << e.what() << endl;
-    }
-
-
-
-    try{
-
-        if (i > 3) throw index_out_of_bounds;
-
-        arr[i] = z;
-
-        for (i = 0; i < 4; i++)
-            cout << "arr[" << i << "] = " << arr[i] << endl;
-    }
-    catch (exception& e){
-
-        cout << "Exceptie: " << e.what() << endl;
-    }
+    cout << A.Find(19) << '\n';
+    cout << A.Find(19, desc) << '\n';
+    //cout << A.Find(19, point1) << '\n';
+    
+    cout << A.GetSize() << ' ' << A.GetCapacity() << '\n';
+    
 
 
     return 0;
